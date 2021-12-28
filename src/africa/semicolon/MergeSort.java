72136e2e2f4f77268956a388932df577ab3eefcd
kindyluv <onyeukwuamara@gmail.com>
2021-12-28 22:46:04 +0100

    ..

--- src/africa/semicolon/MergeSort.java
+++ src/africa/semicolon/MergeSort.java
@@ -4,54 +4,58 @@ public class MergeSort {
 
     public static void mergeSort(int[] array, int lowerBound, int upperBound) {
 
-        if(lowerBound < upperBound){
+        if(lowerBound - upperBound<2){
             return;
         }
 
         int mid = (lowerBound + upperBound)/2;
+        mergeSort(array, lowerBound, mid);
+        mergeSort(array, mid, upperBound);
+        merge(array, lowerBound, mid, upperBound);
 
 
-        merge(array, mid, lowerBound, upperBound);
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
     }
 
-    public static void merge(int[] array, int mid, int lowerBound, int upperBound){
+    public static void merge(int[] array, int lowerBound, int mid, int upperBound) {
 
-        if(array[mid-1]<= array[mid]){
+        if (array[mid - 1] <= array[mid]) {
             return;
         }
 
-        int temp = 0;
+       int temp = 0;
         int start = lowerBound;
-        int end = upperBound;
-        int midPoint = mid+1;
-        int[] b = new int[end-start];
+        int midPoint = mid;
 
-        while(start <= mid && midPoint <= upperBound){
-           if(array[start] <= array[end]){
-               b[temp] = array[start];
-               start++;
-           }else{
-               b[temp] = array[end];
-               end++;
-           }temp++;
+        int[] b = new int[upperBound-lowerBound];
+
+//        while (start <= mid && midPoint <= upperBound) {
+//           if(array[start] <= array[end]){
+//               b[temp] = array[start];
+//               start++;
+//           }else{
+//               b[temp] = array[end];
+//               end++;
+//           }temp++;
+//        }
+//        if (start > mid){
+//            while (end <= upperBound){
+//                b[temp] = array[end];
+//                end++;
+//                temp++;
+//            }
+//        }else{
+//            while(start <= mid){
+//                b[temp] = array[start];
+//                start++;
+//                temp++;
+//            }
+//        }
+//        }int
+        while(start < mid && midPoint < upperBound){
+            b[temp++] = array[start] <= array[midPoint] ? array[start++] : array[midPoint++];
         }
-        if (start > mid){
-            while (end <= upperBound){
-                b[temp] = array[end];
-                end++;
-                temp++;
-            }
-        }else{
-            while(start <= mid){
-                b[temp] = array[start];
-                start++;
-                temp++;
-            }
-        }
+        System.arraycopy(array, start, array, lowerBound + temp, mid -1);
+        System.arraycopy(b, 0, array, lowerBound, temp);
     }
 
 }
